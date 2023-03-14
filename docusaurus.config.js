7126c2632b91adccf0eafdb36bca7daa4e9a3d79
AndrewChao <andrew33673367@gmail.com>
2023-03-14 20:03:21 +0800

    嘗試加入github action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,21 +6,21 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'AndrewYEELife Blog',
+  tagline: '我的部落格，使用Docusaurs Framework建置',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://andrewyeelife.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'AndrewYEELife', // Usually your GitHub org/user name.
+  projectName: 'andrewyeelife.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
